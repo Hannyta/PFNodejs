fe4e14ed9fd2329a7056a66a1b05d09ecc556576
Hanny <Hannylinares@gmail.com>
2025-07-20 14:29:53 -0300

    Instalación de las dependencias CORS, JWT, BODY- PARSER, DOTENV Y FIREBASE

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenidos a la API REST"})
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log (`http://localhost:${PORT}`));
